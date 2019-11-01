f44dc9c3c2caeb3a4d4f9d284b71abfc53c8017b
Dayeon <dayeon.dev@gmail.com>
2019-11-01 22:10:04 +0900

    Practice Layout

--- ex03_layouts/lib/main.dart
+++ ex03_layouts/lib/main.dart
@@ -1,33 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp(
-    //   title: 'Flutter Layout Demo',
-    //   home: Scaffold(
-    //     appBar: AppBar(
-    //       title: Text('Flutter Layout Demo'),
-    //     ),
-    //     body: Center(
-    //       child: Text('Hello World'),
-    //     ),
-    //   ),
-    // );
-    return Container(
-      decoration: BoxDecoration(color: Colors.white),
-      child: Center(
-        child: Text(
-          'Hello Dayeon',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 32,
-            color: Colors.black87,
+    debugPaintSizeEnabled = true;
+    return buildMaterialApp();
+  }
+
+  Widget buildNonMaterialApp() => Container(
+        decoration: BoxDecoration(color: Colors.white),
+        child: Center(
+          child: Text(
+            'Hello Dayeon',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 32,
+              color: Colors.black87,
+            ),
           ),
         ),
-      ),
-    );
-  }
+      );
+
+  Widget buildMaterialApp() => MaterialApp(
+        title: 'Flutter Layout Demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter Layout Demo'),
+          ),
+          body: Center(
+            // child: Text('Hello World'),
+            child: buildExpandedRow(),
+          ),
+        ),
+      );
+
+  Widget buildRow() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/pic1.jpg'),
+          Image.asset('images/pic2.jpg'),
+          Image.asset('images/pic3.jpg'),
+        ],
+      );
+
+  Widget buildColumn() => Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/pic1.jpg'),
+          Image.asset('images/pic2.jpg'),
+          Image.asset('images/pic3.jpg'),
+        ],
+      );
+
+  Widget buildExpandedRow() => Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Image.asset('images/pic1.jpg'),
+          ),
+          Expanded(
+            flex: 2,
+            child: Image.asset('images/pic2.jpg'),
+          ),
+          Expanded(
+            child: Image.asset('images/pic3.jpg'),
+          ),
+        ],
+      );
+
+  Widget buildPackingWidgets() => Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      
+      Icon(Icons.star, color: Colors.green[500]),
+      Icon(Icons.star, color: Colors.green[500]),
+      Icon(Icons.star, color: Colors.green[500]),
+      Icon(Icons.star, color: Colors.black),
+      Icon(Icons.star, color: Colors.black),
+    ],
+  );
 }
