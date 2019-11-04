1309deb8f12f4530272f60c00c48093dc01c6467
Dayeon <dayeon.dev@gmail.com>
2019-11-04 12:34:37 +0900

    Complet food example

--- ex03_layouts/lib/main.dart
+++ ex03_layouts/lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    debugPaintSizeEnabled = true;
+    // debugPaintSizeEnabled = true;
     return buildMaterialApp();
   }
 
@@ -26,15 +26,16 @@ class MyApp extends StatelessWidget {
 
   Widget buildMaterialApp() => MaterialApp(
         title: 'Flutter Layout Demo',
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter Layout Demo'),
-          ),
-          body: Center(
-            // child: Text('Hello World'),
-            child: buildExpandedRow(),
-          ),
-        ),
+        // home: Scaffold(
+        //   appBar: AppBar(
+        //     title: Text('Flutter Layout Demo'),
+        //   ),
+        //   body: Center(
+        //     // child: Text('Hello World'),
+        //     child: buildPackingWidgets(),
+        //   ),
+        // ),
+        home: buildHomePage('Strawberry Pavlova Recipe'),
       );
 
   Widget buildRow() => Row(
@@ -72,14 +73,164 @@ class MyApp extends StatelessWidget {
       );
 
   Widget buildPackingWidgets() => Row(
-    mainAxisSize: MainAxisSize.min,
-    children: [
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.star, color: Colors.green[500]),
+          Icon(Icons.star, color: Colors.green[500]),
+          Icon(Icons.star, color: Colors.green[500]),
+          Icon(Icons.star, color: Colors.black),
+          Icon(Icons.star, color: Colors.black),
+        ],
+      );
+
+  Widget buildHomePage(String title) {
+    final titleText = Container(
+      padding: EdgeInsets.all(20),
+      child: Text(
+        'Strawberry Pavlova',
+        style: TextStyle(
+          fontWeight: FontWeight.w800,
+          letterSpacing: 0.5,
+          fontSize: 30,
+        ),
+      ),
+    );
+
+    final subTitle = Text(
+      'Pavlova is a meringue-based dessert named after the Russian ballerina '
+      'Anna Pavlova. Pavlova features a crisp crust and soft, light inside, '
+      'topped with fruit and whipped cream.',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontFamily: 'Georgia',
+        fontSize: 25,
+      ),
+    );
+
+    final stars = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.black),
+        Icon(Icons.star, color: Colors.black),
+      ],
+    );
+
+    final ratings = Container(
       
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.black),
-      Icon(Icons.star, color: Colors.black),
-    ],
-  );
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          stars,
+          Text(
+            '170 Reviews',
+            style: TextStyle(
+              //Text 설정은 TextStyle로 한다.
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
+          )
+        ],
+      ),
+    );
+
+    // iconList에 사용될 TextStyle 지정
+    final descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    );
+
+    final iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Icon(
+                  Icons.kitchen,
+                  color: Colors.green[500],
+                ),
+                Text('PERP:'),
+                Text('25 min')
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.timer,
+                  color: Colors.green[500],
+                ),
+                Text('COOK:'),
+                Text('1 hr')
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.restaurant,
+                  color: Colors.green[500],
+                ),
+                Text('FEEDS:'),
+                Text('4~6')
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+
+    final leftColumn = Container(
+      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+      child: Column(
+        children: [
+          titleText,
+          subTitle,
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+
+    final mainImage = Image.asset(
+      'images/pavlova.jpg',
+      fit: BoxFit.cover,
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
+          height: 600,
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 440,
+                  child: leftColumn,
+                ),
+                mainImage,
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
