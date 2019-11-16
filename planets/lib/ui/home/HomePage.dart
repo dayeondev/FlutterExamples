524a73b3fd69fafad07aa09b8289585da2f4647b
dayeondev <dayeon.dev@gmail.com>
2019-11-16 23:27:29 +0900

    add Plants Example
    https://sergiandreplace.com/planets-flutter-from-design-to-app/

---
+++ planets/lib/ui/home/HomePage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:planets/ui/home/GradientAppBar.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Column(
+        children: <Widget>[
+          new GradientAppBar("treva"),
+        ],
+      ),
+    );
+  }
+}
